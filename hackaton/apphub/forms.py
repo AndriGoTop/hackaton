5ebe91a1d3854ebb9d4fe33c965636334285f78a
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-04-05 00:18:02 +0300

    forms.py

--- hackaton/apphub/forms.py
+++ hackaton/apphub/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
+from django.utils.safestring import mark_safe
 
 
 class CustomUserRegistrationForm(UserCreationForm):
@@ -9,10 +10,24 @@ class CustomUserRegistrationForm(UserCreationForm):
         fields = ['login', 'email', 'password1', 'password2', "telegram_id"]
 
     def clean_login(self):
-        login = self.cleaned_data['login']
+        login = self.cleaned_data.get('login')
         if CustomUser.objects.filter(login=login).exists():
-            raise forms.ValidationError("Пользователь с таким логином уже существует.")
+            msg = mark_safe(
+                'Пользователь с таким логином уже существует. '
+                '<a href="login/">Войти</a>')
+            raise forms.ValidationError(msg)
         return login
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if CustomUser.objects.filter(email=email).exists():
+            msg = mark_safe(
+                'Пользователь с такой почтой уже зарегистрирован. '
+                '<a href="login/">Войти</a>'
+            )
+            raise forms.ValidationError(msg)
+        return email
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label="Логин")
