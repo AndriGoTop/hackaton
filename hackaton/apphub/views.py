0d168aac539d45b01070e806c40389efd48e9c09
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-04-04 23:59:51 +0300

    models.py

--- hackaton/apphub/views.py
+++ hackaton/apphub/views.py
@@ -2,26 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import CustomUserRegistrationForm, CustomAuthenticationForm
 
+
 def index(request):
     context = {
 
     }
     return render(request, 'apphub/index.html', context)
 
+
 def logout_view(request):
     logout(request)
     return redirect("login")
 
+
 def register_view(request):
     if request.method == "POST":
         form = CustomUserRegistrationForm(request.POST)
+
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("home")
     else:
         form = CustomUserRegistrationForm()
-        return render(request, "main/register.html", {'form':form})
+        return render(request, "main/register.html", {'form': form})
+
+
 def login_view(request):
     if request.method == "POST":
         form = CustomAuthenticationForm(request, data=request.POST)
