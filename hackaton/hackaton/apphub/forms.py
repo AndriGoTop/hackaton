df3a1bb61affddd427e493cb59667617470ff3c0
AndriGoTop <a.golubn@gmail.com>
2025-04-06 11:38:10 +0300

    .

--- hackaton/hackaton/apphub/forms.py
+++ hackaton/hackaton/apphub/forms.py
@@ -15,26 +15,6 @@ class CustomUserRegistrationForm(UserCreationForm):
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', "telegram_id"]
 
-<<<<<<< HEAD:hackaton/hackaton/apphub/forms.py
-    def clean_login(self):
-        username = self.cleaned_data.get('username')
-        if CustomUser.objects.filter(username=username).exists():
-            msg = mark_safe(
-                'Пользователь с таким логином уже существует. '
-                '<a href="login/">Войти</a>')
-            raise forms.ValidationError(msg)
-        return username
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if CustomUser.objects.filter(email=email).exists():
-            msg = mark_safe(
-                'Пользователь с такой почтой уже зарегистрирован. '
-                '<a href="login/">Войти</a>'
-            )
-            raise forms.ValidationError(msg)
-        return email
-=======
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
@@ -42,7 +22,6 @@ class CustomUserRegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
->>>>>>> on_develop:hackaton/apphub/forms.py
 
 
 class CustomAuthenticationForm(AuthenticationForm):
