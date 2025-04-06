f63e90f5c5482e1c982ac20c3182f1d6e628561f
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-04-06 06:30:12 +0300

    DEL

--- hackaton/hackaton/apphub/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser,News,Tags,Subs
-admin.site.register(CustomUser)
-admin.site.register(News)
-admin.site.register(Tags)
-admin.site.register(Subs)
-
