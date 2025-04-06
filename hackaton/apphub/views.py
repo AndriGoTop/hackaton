47b9190cd7bd2bd4c8240d025238dd2ae3e923c0
AndriGoTop <a.golubn@gmail.com>
2025-04-06 11:08:58 +0300

    relese

--- hackaton/apphub/views.py
+++ hackaton/apphub/views.py
@@ -1,37 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import CustomUserRegistrationForm, CustomAuthenticationForm, ProfileForm
+from django.views.generic import DetailView
 from .models import News
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    first_news = News.objects.filter(is_published=True).order_by('created_at')[:7]
-    reg_form = CustomUserRegistrationForm()
-    login_form = CustomAuthenticationForm(request, data=request.POST or None)
-    if request.method == "POST":
-        if 'register' in request.POST:
-            reg_form = CustomUserRegistrationForm(request.POST)
-            if reg_form.is_valid():
-                user = reg_form.save(commit=False)
-                user.set_password(reg_form.cleaned_data['password1'])
-                print(reg_form.errors)
-                user.save()
-                login(request, user)
-                messages.success(request, "Регистрация прошла успешно.")
-                return redirect('index')
+    first_news = News.objects.filter(is_published=True).order_by('created_at')[:6]
+    context = {
 
-        elif 'login' in request.POST:
-            login_form = CustomAuthenticationForm(request, data=request.POST)
-            if login_form.is_valid():
-                user = login_form.get_user()
-                login(request, user)
-                messages.success(request, "Вы вошли в систему.")
-                return redirect('index')
-            else:
-                messages.error(request, "Неверный логин или пароль.")
+    }
+    return render(request, 'apphub/index.html', {'news_list': first_news})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+    else:
+        form = CustomUserRegistrationForm()
+    return render(request, "apphub/register.html", {'form': form})
 
     return render(request, 'apphub/index.html', {
         'reg_form': reg_form,
@@ -39,15 +37,22 @@ def index(request):
         'news_list': first_news,
     })
 
+def login_view(request):
+    if request.method == "POST":
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")
+    else:
+        form = CustomUserRegistrationForm()
+    return render(request, "apphub/login.html", {'form': form})
 
-def logout_view(request):
-    logout(request)
-    return redirect("index")
 
-
-def NewsDetailView(request, pk):
-    news = get_object_or_404(News, pk=pk, is_published=True)
-    return render(request, 'apphub/news_detail.html', {'news': news})
+class NewsDetailView(DetailView):
+    model = News
+    template_name = 'news_detail.html'
+    context_object_name = 'news'
 
 
 @login_required
